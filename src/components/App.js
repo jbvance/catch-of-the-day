b449669316c9cf39765fea7c50d828b2acc39fa2
Jason Vance <jbvance@gmail.com>
2017-03-29 12:22:03 -0500

    Begin add to order functionality

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ class App extends React.Component{
     super();
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
 
     //getInitialState
     this.state = {
@@ -35,6 +36,16 @@ loadSamples(){
   });
 }
 
+addToOrder(key){
+  //take a copy of our state
+  const order = {...this.state.order};
+  //update or add new number of fish ordered
+  order[key] = order[key] + 1 || 1;
+  this.setState({ order });
+  //Could also say this.setState({ order: order });
+
+}
+
   render(){
     return (
       <div className="catch-of-the-day">
@@ -44,7 +55,7 @@ loadSamples(){
             {
               Object
               .keys(this.state.fishes)
-              .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+              .map(key => <Fish key={key} index={key} details={this.state.fishes[key]}  addToOrder={this.addToOrder}/>)
             }
           </ul>
         </div>
