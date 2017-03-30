0b3d6811c90dfeb939bbcf4532b5a0d0be3d2a3a
Jason Vance <jbvance@gmail.com>
2017-03-30 11:48:42 -0500

    Add live editing of inventory state

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,8 @@ class App extends React.Component{
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+    this.updateFish = this.updateFish.bind(this);
+
 
     //getInitialState
     this.state = {
@@ -58,6 +60,13 @@ addFish(fish){
     //could also use this.setState({ fishes: fishes })
 }
 
+updateFish(key, updatedFish){
+  const fishes = {...this.state.fishes};
+  fishes[key] = updatedFish;
+  this.setState({ fishes });
+  //could also use this.setState({fishes: fishes })
+}
+
 loadSamples(){
   this.setState({
     fishes: sampleFishes
@@ -91,7 +100,12 @@ addToOrder(key){
           fishes={this.state.fishes}
           order={this.state.order}
           params={this.props.params} />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+        <Inventory
+          addFish={this.addFish}
+          loadSamples={this.loadSamples}
+          fishes={this.state.fishes}
+          updateFish={this.updateFish}
+        />
       </div>
     );
   }
